e27ac813a312ee201998d65637ca4933fe2a27c2
EmadBrown <emadbrown2013@gmail.com>
2017-12-07 17:01:51 +0100

    Create UserObserver / ObserverServiceProvider for inserting student_number / access_token

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+use Illuminate\Support\Carbon;
+
+class UserObserver
+{
+    /**
+     * Listen to the User created event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function creating(User $user)
+    {
+        // add access token to users table
+         $user->access_token = bin2hex(openssl_random_pseudo_bytes(20));
+       
+        // add student number to users table
+        $users = User::all()->count();
+        $date = Carbon::now();
+        $user->student_number = $date->year. '' .$date->month. '' . $users + 1;
+    }
+
+    /**
+     * Listen to the User deleting event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleting(User $user)
+    {
+        //
+    }
+}
--- config/app.php
+++ config/app.php
@@ -176,7 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        // Config Observer Service Povider
+        App\Providers\ObserverServiceProvider::class,
     ],
 
     /*
